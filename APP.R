807f7603901ab1bbfbfe8a1a31e094e09ce460b5
chengxi520 <36138917+chengxi520@users.noreply.github.com>
2018-02-19 18:05:01 -0500

    Add files via upload

---
+++ APP.R
@@ -0,0 +1,6 @@
+
+rsconnect::setAccountInfo(name='chengxi',
+                          token='DBA921FB2FEF7076EB4C6645032C245D',
+                          secret='8pvLmHlWpvXV8ryR50C7s/jgG2iIl9WulH9aKOnA')
+library(rsconnect)
+deployApp()
